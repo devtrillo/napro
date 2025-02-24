cd92908120ec75d7178c4b960641597304d3d595
Esteban Trillo <esteban@devtrillo.com>
2025-02-24 12:47:42 -0600

    add the button

--- src/routes/(marketing)/+page.svelte
+++ src/routes/(marketing)/+page.svelte
@@ -1,6 +1,10 @@
+<script lang="ts">
+  import Button from '$lib/components/ui/button.svelte';
+</script>
+
 <h1 class=" text-2xl">Welcome to SvelteKit</h1>
 
 <p>
-  Visit <a class="btn btn-primary" href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read
-  the documentation
+  Visit <Button color="secondary" href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</Button> to
+  read the documentation
 </p>
