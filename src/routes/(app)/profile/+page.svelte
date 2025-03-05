62b8088c77dd4a92391d15384af417d7cfca1a63
Esteban Trillo <esteban@devtrillo.com>
2025-03-05 13:54:01 -0600

    fix: links

--- src/routes/(app)/profile/+page.svelte
+++ src/routes/(app)/profile/+page.svelte
@@ -21,59 +21,61 @@
   let formChangePassword = $derived(data.formChangePassword);
 </script>
 
-<Card.Root class="container mx-auto">
-  <Card.Header>
-    <Card.Title>User</Card.Title>
-  </Card.Header>
-  <Card.Content class="grid grid-cols-1 gap-8">
-    <div class="flex items-start justify-between">
-      <div class="flex items-center gap-4">
-        <Avatar.Root class="hidden h-9 w-9 sm:flex">
-          <Avatar.Image src={user.image || '#'} alt="Avatar" class="object-cover" />
-          <Avatar.Fallback>{user.name.charAt(0)}</Avatar.Fallback>
-        </Avatar.Root>
-        <div class="grid gap-1">
-          <p class="text-sm leading-none font-medium">
-            {user.name}
-          </p>
-          <p class="text-sm">{user.email}</p>
+<div class="flex h-dvh items-center justify-center">
+  <Card.Root class="w-full max-w-lg">
+    <Card.Header>
+      <Card.Title>User</Card.Title>
+    </Card.Header>
+    <Card.Content class="grid grid-cols-1 gap-8">
+      <div class="flex items-start justify-between">
+        <div class="flex items-center gap-4">
+          <Avatar.Root class="hidden h-9 w-9 sm:flex">
+            <Avatar.Image src={user.image || '#'} alt="Avatar" class="object-cover" />
+            <Avatar.Fallback>{user.name.charAt(0)}</Avatar.Fallback>
+          </Avatar.Root>
+          <div class="grid gap-1">
+            <p class="text-sm leading-none font-medium">
+              {user.name}
+            </p>
+            <p class="text-sm">{user.email}</p>
+          </div>
+        </div>
+        <EditUserDialog form={formEditProfile} />
+      </div>
+      <VerifyEmail {user} />
+      <div class="flex w-max flex-col gap-1 border-l-2 px-2">
+        <p class="text-xs font-medium">Active Sessions</p>
+        {#await data.activeSessions}
+          <span class="flex items-center gap-2"> <LoaderIcon size={20} /> Loading sessions...</span>
+        {:then activeSessions}
+          {#each activeSessions?.filter((session) => session.userAgent) as session (session.id)}
+            <Session {session} currentSession={data.session} />
+          {/each}
+        {/await}
+      </div>
+      <div class="flex flex-wrap items-center justify-between gap-2 border-y py-4">
+        <div class="flex flex-col gap-2">
+          <p class="text-sm">Passkeys</p>
+          <div class="flex flex-wrap gap-2">
+            <AddPasskey form={formAddNewPasskey} />
+            <ListPasskey />
+          </div>
+        </div>
+        <div class="flex flex-col gap-2">
+          <p class="text-sm">Two Factor</p>
+          <div class="flex gap-2">
+            {#if user.twoFactorEnabled}
+              <TwoFactorDisable />
+            {:else}
+              <TwoFactorEnabled />
+            {/if}
+          </div>
         </div>
       </div>
-      <EditUserDialog form={formEditProfile} />
-    </div>
-    <VerifyEmail {user} />
-    <div class="flex w-max flex-col gap-1 border-l-2 px-2">
-      <p class="text-xs font-medium">Active Sessions</p>
-      {#await data.activeSessions}
-        <span class="flex items-center gap-2"> <LoaderIcon size={20} /> Loading sessions...</span>
-      {:then activeSessions}
-        {#each activeSessions?.filter((session) => session.userAgent) as session (session.id)}
-          <Session {session} currentSession={data.session} />
-        {/each}
-      {/await}
-    </div>
-    <div class="flex flex-wrap items-center justify-between gap-2 border-y py-4">
-      <div class="flex flex-col gap-2">
-        <p class="text-sm">Passkeys</p>
-        <div class="flex flex-wrap gap-2">
-          <AddPasskey form={formAddNewPasskey} />
-          <ListPasskey />
-        </div>
-      </div>
-      <div class="flex flex-col gap-2">
-        <p class="text-sm">Two Factor</p>
-        <div class="flex gap-2">
-          {#if user.twoFactorEnabled}
-            <TwoFactorDisable />
-          {:else}
-            <TwoFactorEnabled />
-          {/if}
-        </div>
-      </div>
-    </div>
-  </Card.Content>
-  <Card.Footer class="flex items-center justify-between gap-2">
-    <ChangePassword form={formChangePassword} />
-    <SignOut />
-  </Card.Footer>
-</Card.Root>
+    </Card.Content>
+    <Card.Footer class="flex items-center justify-between gap-2">
+      <ChangePassword form={formChangePassword} />
+      <SignOut />
+    </Card.Footer>
+  </Card.Root>
+</div>
