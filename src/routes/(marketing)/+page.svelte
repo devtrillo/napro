e2b35e07fe0e38462d9cd678ab0db96a17d267c4
Esteban Trillo <esteban@devtrillo.com>
2025-09-08 11:19:20 -0600

    feat: add i18n

--- src/routes/(marketing)/+page.svelte
+++ src/routes/(marketing)/+page.svelte
@@ -1,8 +1,9 @@
 <script>
 	import { Button } from '$lib/components/ui/button';
+	import { m } from '$lib/paraglide/messages';
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<Button>click me</Button>
+<Button>{m.hi()}</Button>
